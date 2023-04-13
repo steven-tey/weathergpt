5a2e40a85f173201168d4901e5a063f83cc0d0ad
Steven Tey <stevensteel97@gmail.com>
2023-04-13 11:46:53 -0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "WeatherGPT - ChatGPT Plugin to get the weather of any given location",
@@ -23,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
