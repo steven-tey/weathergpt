b3cb11727ad0ee972b7d4c74388e8b3deb2fd96b
Steven Tey <stevensteel97@gmail.com>
2023-04-12 21:42:00 -0500

    Introducing WeatherGPT

---
+++ app/api/weather/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { geolocation } from "@vercel/edge";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function GET(req: NextRequest) {
+  let location = req.nextUrl.searchParams.get("location");
+  if (!location) {
+    const { city } = geolocation(req);
+    location = city || "San Francisco";
+  }
+
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${location}&aqi=no`
+  ).then((res) => res.json());
+
+  return NextResponse.json(response);
+}
